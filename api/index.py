39592ea0ff69e290944f3afcd2fec9618f911b88
artificialintelligence3070-oss <artificialintelligence3070@gmail.com>
2026-07-10 23:35:30 +0530

    Change MASTER_KEY in index.py
    Updated MASTER_KEY for API authentication.

--- api/index.py
+++ api/index.py
@@ -12,7 +12,7 @@ app = FastAPI(title="SHAYAN_EXPLORER HUB API")
 
 # --- CONFIGURATION & SECURITY ---
 TARGET_BASE_API = "https://ft-osint-api.duckdns.org/api"
-MASTER_KEY = "shayan-exploindia"
+MASTER_KEY = "vernex16"
 ADMIN_USER = "vernex"
 ADMIN_PASS = "vernex@16vx"
 
